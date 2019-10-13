26db29c328848cc52dcff497d546b20b752b837a
Martin Seeler <developer@chasmo.de>
2019-10-13 12:37:14 +0200

    Updates

---
+++ precache-manifest.fa4f61a77407327ef600ddf5050e1cda.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b327c4ca17dc2ff8406dc401a2d1dd16",
+    "url": "/index.html"
+  },
+  {
+    "revision": "96c857694c54b8808cd7",
+    "url": "/static/css/main.dfca195d.chunk.css"
+  },
+  {
+    "revision": "a5d1097ded60e0ba2fc0",
+    "url": "/static/js/2.c30e3cb9.chunk.js"
+  },
+  {
+    "revision": "96c857694c54b8808cd7",
+    "url": "/static/js/main.6579c055.chunk.js"
+  },
+  {
+    "revision": "6c691631be18485bb588",
+    "url": "/static/js/runtime-main.7db19a64.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/static/media/logo.25bf045c.svg"
+  }
+]);
